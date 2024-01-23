889c1051c9bbfe43cb4ae40a507aa0758685a27c
SilverDazt <dimasabimanyu1099@gmail.com>
2024-01-23 19:41:48 +0700

    first commit

---
+++ music-list.js
@@ -0,0 +1,112 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+
+    name: "01. Style",
+    artist: "Taylor Swift",
+    img: "04",
+    src: "01styletaylor"
+  },
+  {
+    name: "02. Come Inside Of My Hearth",
+    artist: "IV Of Spades",
+    img: "04",
+    src: "02comeinside"
+  },
+  {
+    name: "03. I Wanna Be Yours",
+    artist: "Arctic Monkeys",
+    img: "04",
+    src: "03iwannabe"
+  },
+  {
+    name: "04. W.H.U.T",
+    artist: "Aisha Retno",
+    img: "04",
+    src: "04whut"
+  },
+  {
+    name: "05. Every Summertime",
+    artist: "NIKI",
+    img: "04",
+    src: "05summertime"
+  },
+  {
+    name: "06. Jatuh Hati",
+    artist: "Raisa",
+    img: "04",
+    src: "06jatuhhati"
+  },
+  {
+
+    name: "07. Dia",
+    artist: "MALIQ & D'Essentials",
+    img: "04",
+    src: "07dia"
+  },
+  {
+    name: "08. Anything You Want",
+    artist: "Reality Club",
+    img: "04",
+    src: "08anything"
+  },
+  {
+    name: "09. Teka Teki",
+    artist: "Raisa",
+    img: "04",
+    src: "09tekateki"
+  },
+  {
+    name: "10. P.S I LOVE YOU",
+    artist: "Paul Partohap",
+    img: "04",
+    src: "10psiloveyou"
+  },
+  {
+    name: "11. Old Love",
+    artist: "yuji, putri dahlia",
+    img: "04",
+    src: "11oldlove"
+  },
+  {
+    name: "12. I Like U",
+    artist: "NIKI",
+    img: "04",
+    src: "12ilikeu"
+  },
+  {
+    name: "13. Sweet",
+    artist: "NIKI",
+    img: "04",
+    src: "13sweet"
+  },
+  {
+    name: "14. IT'S YOU (feat. keshi)",
+    artist: "MAX, keshi",
+    img: "04",
+    src: "14itsyou"
+  },
+  {
+    name: "15. Those Eyes",
+    artist: "New West",
+    img: "04",
+    src: "15thoseyes"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
